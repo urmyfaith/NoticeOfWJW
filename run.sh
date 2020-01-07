569ed8b69d05f248d1b17f3099245e7d697868c5
zuoxue <zuoxue@qq.com>
2020-01-07 10:40:23 +0800

    save

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ curl -s http://wjw.wuhan.gov.cn/front/web/list2nd/no/710 >temp.html
 
 cat temp.html |  grep "showDetail" | awk -F "[\"\"]" '{print $4}' > latest.txt
 
-diff -B  <(cat latest.txt)  <(cat lines.txt)
 msg=`diff -B  <(cat latest.txt)  <(cat lines.txt) | tail -n1`
 
 if test -z "$msg"
@@ -13,5 +12,9 @@ then
 else  
   msg=`echo $msg | awk '{print $2}'`
   #echo $msg
-  terminal-notifier -group 'zxReminder' -title 'News' -message $msg
+  time=`cat temp.html |  grep "time" |  awk -F "[\>\<]" '{print $3}' | head -n1`
+  #echo $time
+  link=`cat temp.html |  grep "showDetail" | awk -F "[\"\"]" '{print $2}' | head -n1`
+  curl -s "$link" >msg.html
+  terminal-notifier -group 'zxReminder' -title 'News' -subtitle "$msg"  -message "$time"  -open "$link"
 fi
